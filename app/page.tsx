3898263d1b127b248a1d15a3f7dd1fc2fe2b5cf3
gtdotwav <167007459+gtdotwav@users.noreply.github.com>
2025-08-01 12:10:04 -0300

    Fix searchParams invariant error

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@
 import { Suspense } from "react"
 import HomePage from "@/components/home-page"
 
-export default function Page() {
+export default function Page({ 
+  searchParams 
+}: { 
+  searchParams?: { [key: string]: string | string[] | undefined } 
+}) {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <HomePage />
